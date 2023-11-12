d651a9f63c5fe99ab823e57f0f3edfde997bb35c
arezkitakfa <arezkitakfa@gmail.com>
2023-11-12 22:28:39 +0100

    Fix port Front

--- f2i-project-Api/src/main.ts
+++ f2i-project-Api/src/main.ts
@@ -9,7 +9,7 @@ import { IsAdminPipe } from './users/pipe/is-admin.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
- origin: 'http://localhost:80',
+ origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
