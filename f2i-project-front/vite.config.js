29e19be8e0d4be4e5b63bc046d0d224f8c05dfc5
hamidaniarezkitakfa <arezkitakfa@gmail.com>
2023-12-13 10:21:09 +0100

    add admin

--- f2i-project-front/vite.config.js
+++ f2i-project-front/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias: {
+    '@mui/styled-engine': '@mui/styled-engine-sc',
+  },
 })
